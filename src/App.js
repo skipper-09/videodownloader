77796d7f886a90a1f2a2a196a0063a3b4c1cb7e6
skipper-09 <dxdskipper@gmail.com>
2023-03-14 07:14:21 +0700

    first add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [link, Setlink] = useState("");
+  const [currenlink, SetCurrentlink] = useState("");
+
+
+  function ChangeUrl(e) {
+    Setlink(e.target.value)
+  }
+
+  function HandleCurrentLink() {
+    SetCurrentlink(link);
+    Setlink("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='container'>
+
+        <div>
+          <h3 className='text-uppercase text-center mt-4 fw-bold header-title'>All in one Downloader</h3>
+        </div>
+        <div className="d-flex p-3 ">
+          <input type="text"
+            class="form-control mx-2" name="" id="" aria-describedby="helpId" placeholder="Tempel Url disini" value={link}
+            onChange={ChangeUrl}
+          />
+          <a class="btn " href="#" role="button" onClick={HandleCurrentLink}>Download</a>
+        </div>
+        <iframe id="widgetv2Api" src={`http://convert2mp3s.com/api/widgetv2?url=${currenlink}`} scrolling={"yes"}></iframe>
+      </div>
+    </>
   );
 }
 
